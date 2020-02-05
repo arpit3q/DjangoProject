52ab2509c46d8c7c1dc1a552dc3bde71e2b4e329
Arpit Rai <arpit3qq@gmail.com>
2020-02-05 11:35:00 +0530

    Generic views

--- polls/views.py
+++ polls/views.py
@@ -4,22 +4,20 @@ from django.shortcuts import render,get_object_or_404
 from django.template import loader
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
-
+from django.views import generic
 from .models import Question,Choice
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:]
-    context={
-        "latest_question_list":latest_question_list
-    }
-    return render(request,"polls/index.html",context)
-
-def detail(request,question_id):
-    question=get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/detail.html",{"question":question})
-def results(request,question_id):
-    question=get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
-
+from django.db.models import F
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:]
+class DetailView(generic.DetailView):
+    model=Question
+    template_name='polls/detail.html'
+class ResultsView(generic.DetailView):
+    model=Question
+    template_name='polls/results.html'
 def vote(request,question_id):
     question=get_object_or_404(Question,pk=question_id)
     try:
@@ -29,7 +27,7 @@ def vote(request,question_id):
             'question':question,'error_message':"You didn't select a choice",
         })
     else:
-        selected_choice.votes+=1
+        selected_choice.votes=F('votes')+1
         selected_choice.save()
         # return render(request,'polls/results.html',{
         #     'question':question,'error_message':"You didn't select a choice",
